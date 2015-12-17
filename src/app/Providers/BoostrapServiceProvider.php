c10c684fd3083c011b04dc8e0a56c3bfda9bc96b
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2015-12-17 16:53:35 +0100

    First version

---
+++ src/app/Providers/BoostrapServiceProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Acacha\LaravelBootstrap\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * Class BootstrapServiceProvider
+ *
+ * @package Acacha\LaravelBootstrap\Providers
+ */
+class BootstrapServiceProvider extends ServiceProvider
+{
+
+    /**
+     * The main bootstrap file. The first file readed in config('bootstrap.directory')
+     *
+     */
+    protected $file;
+
+    /**
+     * The bootstrap directory
+     *
+     */
+    protected $directory;
+
+    /**
+     * Register Service provider
+     *
+     */
+    public function register()
+    {
+        $this->directory = config('bootstrap.directory','bootstrap');
+        $this->file = config('bootstrap.file','bootstrap.php');
+        if ( ! is_dir($this->directory))
+        {
+            return;
+        }
+        $files = $this->getAllPhpFiles();
+        foreach ($files as $file)
+        {
+            require $file;
+        }
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishConfigFile();
+    }
+
+    /**
+     * get all PHP files in boostrap directory
+     *
+     * @return Finder
+     */
+    protected function getAllPhpFiles()
+    {
+        $files = Finder::create()->files()->name('/^.+\.php$/')->in($this->directory);
+        $files->sort(function ($a)
+        {
+            return $a->getFilename() !== $this->file;
+        });
+        return $files;
+    }
+
+    /**
+     * Publish config file
+     */
+    protected function publishConfigFile()
+    {
+        $this->publishes([
+            dirname(__FILE__) . '/../../config/bootstrap.php' => config_path('bootstrap.php')
+        ]);
+    }
+
+}
---
+++ src/config/bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | The boostrap directory
+    |--------------------------------------------------------------------------
+    |
+    | The folder from which BootstrapServiceProvider will execute/bootstrap/require
+    | all files with .php extension
+    |
+    */
+
+    'directory' => env('BOOTSTRAP_DIRECTORY', 'bootstrap'),
+
+
+    /*
+   |--------------------------------------------------------------------------
+   | The main file in bootstrap directory
+   |--------------------------------------------------------------------------
+   |
+   | This file will be the first to be executed/required. Other files will be
+   | executed in alphabetical order
+   |
+   */
+
+    'file' => env('BOOTSTRAP_MAIN_FILE', 'bootstrap.php')
+
+];
